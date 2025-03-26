1c51d9e534994ac2f60e59dab9e9df8a102febee
DaniVera-tech <daniss.vera@gmail.com>
2025-03-26 16:10:17 +0100

    Update Home.jsx

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+// Import React into the bundle
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Include bootstrap npm library into the bundle
+import 'bootstrap';
+import './icons.js';
+import SimpleCounter from "./component/simpleCounter.jsx"
 
-import secondCounter from "./SecondsCounter.jsx";
+// Include your index.scss file into the bundle
+import '../styles/index.scss';
 
-//create your first component
-const Home = () => {
-	return (
-		<secondCounter />
-	);
-};
 
-export default Home;
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+setInterval(() => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+    
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
